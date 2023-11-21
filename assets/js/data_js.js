61b0d9a41bdeec95a58e3ba0c5c3e10f18257d75
Idealisoa04 <119027567+Idealisoa04@users.noreply.github.com>
2023-11-21 16:04:28 +0600

    update

--- assets/js/data_js.js
+++ assets/js/data_js.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://catfact.ninja/fact') // Remplacez par l'URL de votre API Spring
+    fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population') // Remplacez par l'URL de votre API Spring
         .then(response => response.json())
         .then(data => {
             const listeDonnees = document.getElementById('list');
 
-            // Boucle sur les données et les ajoute à la liste
-            data.forEach(donnee => {
+            console.log(data);
+            //Boucle sur les données et les ajoute à la liste
+            data.data.forEach(donnee => {
                 const listItem = document.createElement('li');
-                listItem.textContent = `ID: ${donnee.fact}`;
+                listItem.textContent = `ID: ${donnee.Nation}`;
                 listeDonnees.appendChild(listItem);
             });
         })
